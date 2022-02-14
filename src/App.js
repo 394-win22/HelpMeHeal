31732fd3dea79f03c863acc149a114dad960ad06
Zheng Kaixin <kaixinzheng2022@u.northwestern.edu>
2022-02-14 15:55:23 -0600

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import SurveyPage from './components/surveypage';
 import MailTo from './components/emailWidget';
 import ProgressIndicator from './components/ProgressBar';
-
+import './App.css';
 import useStore from './Store';
 
 
@@ -22,12 +22,10 @@ function App() {
 
   return (
     <div className="App">
-      <div className='app-header'>
+      <div className="app-header">
         <img src='https://s2.loli.net/2022/02/15/jvkdyu3NJMc65qT.png'/>
         <h1>Help Me Heal</h1>
       </div>
-
-
 
       <ProgressIndicator />
 
