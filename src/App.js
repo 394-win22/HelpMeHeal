7efc768e1cec7f56abaf5ac9796d60f4e62bf32c
Max Tokman <maximetokman@gmail.com>
2022-02-14 15:08:22 -0600

    Add logo

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className='app-header'>
+        <img src='https://s2.loli.net/2022/02/15/jvkdyu3NJMc65qT.png'/>
         <h1>Help Me Heal</h1>
       </div>
       <MailTo />
