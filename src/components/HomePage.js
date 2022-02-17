60cfdc472ca35c0bda1bc4e3b768883d7b16a74f
sathvik <sathu.sathvik@gmail.com>
2022-02-17 16:54:47 -0600

    moved currentDay

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,6 +4,8 @@ import EmailIcon from '@mui/icons-material/Email';
 import Button from '@mui/material/Button';
 import ProgressIndicator from './ProgressBar';
 import Welcome from './Welcome';
+import calculateDay from '../utilities/calculateday';
+
 
 const buttonStyle = {
     fontWeight: "normal",
@@ -31,13 +33,17 @@ const IconStyle = {
     },
 }
 
-const HomePage = ({ data, name, surgeryType, setPage, activeIndex, setActiveIndex, }) => {
+
+const HomePage = ({ data, currentDay, surgeryType, name, type, setPage, activeIndex, setActiveIndex }) => {
+
 
     return (
         <div>
             {/* the zero bellow has to change userid as its real id later */}
-            <Welcome username={name} surgeryType={surgeryType} firebaseData={data} activeIndex={activeIndex} startdate={data["user"][0]["startDate"]} daysDict={data["surgery"][surgeryType]["days"]} />
-            <ProgressIndicator setActiveIndex={setActiveIndex} startdate={data["user"][0]["startDate"]} phaseEndDay={data["surgery"][surgeryType]["phaseEndDay"]} />
+
+            <Welcome username={name} surgeryType={surgeryType} firebaseData={data} activeIndex={activeIndex} currentDay={currentDay} daysDict={data["surgery"][surgeryType]["days"]} />
+            <ProgressIndicator setActiveIndex={setActiveIndex} currentDay={currentDay} phaseEndDay={data["surgery"][surgeryType]["phaseEndDay"]} />
+
             <Box>
                 <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0, background: '#b43434', flexDirection: 'row', justifyContent: 'center', p: 2 }}>
                     <Button onClick={() => window.location = 'mailto:helpmeheal.project@gmail.com'}>
