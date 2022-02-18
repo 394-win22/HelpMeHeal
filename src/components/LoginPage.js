2595ce96110f5d2dd8071bbeda96a68ece54a522
kaixin1326 <kaixinzheng2022@u.northwestern.edu>
2022-02-17 18:17:32 -0600

    Update LoginPage.js

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,6 +1,11 @@
 import { SignInOut } from "./SignInWithGoogle"
 import React, { useState } from 'react';
 const LoginPage = () => {
+    /*To do 
+        1. get user data from firebase, then find if there is data[user][userEmail]
+        2. if not then go to register page
+        3. if yes then setUser and show userpage
+       */
     const [userEmail, setUserEmail] = useState()
     return (
         <div>
