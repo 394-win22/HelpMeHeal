1b721341aa8375f9d7050d52842dcc28ee1295e3
Max Tokman <maximetokman@gmail.com>
2022-02-22 21:07:41 -0600

    cap welcome message at last day

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -27,18 +27,27 @@ const HomePage = ({ data, currentDay, surgeryType, name, type, setPage, activeIn
     const doctorEmail = data["user"][user.doctorId]["email"];
     return (
         <div>
-            {/* the zero bellow has to change userid as its real id later */}
-            <ProgressIndicator setActiveIndex={setActiveIndex} currentDay={currentDay} phaseEndDay={data["surgery"][surgeryType]["phaseEndDay"]} isMobile={isMobile} />
-            <SwitchDay currentDay={currentDay} setCurrentDay={setCurrentDay} isMobile={isMobile} phaseEndDay={data["surgery"][surgeryType]["phaseEndDay"]}/>
-            <Welcome username={name} surgeryType={surgeryType} firebaseData={data} activeIndex={activeIndex} currentDay={currentDay} daysDict={data["surgery"][surgeryType]["days"]} />
-            
+            <ProgressIndicator setActiveIndex={setActiveIndex} 
+                currentDay={currentDay} 
+                phaseEndDay={data["surgery"][surgeryType]["phaseEndDay"]} 
+                isMobile={isMobile} />
+            <SwitchDay currentDay={currentDay} 
+                setCurrentDay={setCurrentDay} 
+                isMobile={isMobile} 
+                phaseEndDay={data["surgery"][surgeryType]["phaseEndDay"]}/>
+            <Welcome username={name} 
+                surgeryType={surgeryType} 
+                firebaseData={data} 
+                activeIndex={activeIndex} 
+                currentDay={currentDay} 
+                daysDict={data["surgery"][surgeryType]["days"]}
+                phaseEndDay={data["surgery"][surgeryType]["phaseEndDay"]} />
             <NavBar data={data} 
                     // showEmailForm={showEmailForm} 
                     // setShowEmailForm={setShowEmailForm} 
                     // handleShowEmailFormClose={handleShowEmailFormClose} 
                     setPage={setPage}
                     user={user}/>
-
 
         </div>
     )
