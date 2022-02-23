fb7b2b56f5f90afbd9ff2180ffc84f2e03a02b26
kaixin1326 <kaixinzheng2022@u.northwestern.edu>
2022-02-22 20:33:12 -0600

    Update CreateFakePatient.js

--- src/doctor_components/CreateFakePatient.js
+++ src/doctor_components/CreateFakePatient.js
@@ -16,25 +16,29 @@ const buttonStyle = () => ({
     },
 })
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+function getRandomInt(max, offset) {
+    return Math.floor(Math.random() * max + offset);
 }
 
 const CreateFakeData = ({ googleUser }) => {
     const id = "Fake" + getRefByPush('/');
-    const fakesurveydata = { pain_rating: 6, rehab_successful: ['No'], concerns: "I can't finish the rehab exercise, because it is so painful." }
+    const fakesurveydata0 = { pain_rating: getRandomInt(3, 0), rehab_successful: ['Yes'], concerns: "" }
+    const fakesurveydata1 = { pain_rating: getRandomInt(4, 7), rehab_successful: ['No'], concerns: "I can't finish the rehab exercise, because it is so painful." }
+    const fakesurveydata2 = { pain_rating: getRandomInt(3, 0), rehab_successful: ['Yes'], concerns: "" }
+    const fakesurveydata3 = { pain_rating: getRandomInt(4, 7), rehab_successful: ['No'], concerns: "I can't finish the rehab exercise, because it is so painful." }
+    const fakesurveydata4 = { pain_rating: getRandomInt(4, 7), rehab_successful: ['No'], concerns: "I can't finish the rehab exercise, because it is so painful." }
     setData(`/user/${id}/userType`, "patient");
     setData(`/user/${id}/name`, "FakePatient");
-    setData(`/user/${id}/startDate`, Date.now() - 86400000 * 4);
+    setData(`/user/${id}/startDate`, Date.now() - 86400000 * 5);
     setData(`/user/${id}/email`, id + "@email.com");
     setData(`/user/${id}/doctorId`, googleUser?.uid);
     setData(`/user/${id}/surgeryType`, "acl");
     setData(`/user/${googleUser?.uid}/patientId/${id}`, "FakePatient")
-    setData(`/user/${id}/surveyResults/0`, fakesurveydata)
-    setData(`/user/${id}/surveyResults/1`, fakesurveydata)
-    setData(`/user/${id}/surveyResults/2`, fakesurveydata)
-    setData(`/user/${id}/surveyResults/3`, fakesurveydata)
-    setData(`/user/${id}/surveyResults/4`, fakesurveydata)
+    setData(`/user/${id}/surveyResults/0`, fakesurveydata0)
+    setData(`/user/${id}/surveyResults/1`, fakesurveydata1)
+    setData(`/user/${id}/surveyResults/2`, fakesurveydata2)
+    setData(`/user/${id}/surveyResults/3`, fakesurveydata3)
+    setData(`/user/${id}/surveyResults/4`, fakesurveydata4)
 }
 
 const CreateFakePatient = (googleUser) => {
