b41ccde44021183c6657d8e2801d49c5472676c3
kaixin1326 <kaixinzheng2022@u.northwestern.edu>
2022-02-24 23:32:11 -0600

    Restyle login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -10,37 +10,47 @@ const LoginPage = ({ isMobile }) => {
     return (
         <div className="loginPage">
             <div className="navbar">
-                <div className="navbarContent">
-                    <img src="bannerlogo.jpg" alt="help-me-heal-logo" style={{borderRadius:"0.2rem", height:"2rem"}}/>
-                    <span style={{  marginLeft:"1rem",
-                        marginTop:"0.3rem",
-                        paddingRight:"30%",
-                        color:"rgb(142,149,160)"}}><b>Help Me Heal</b></span>
+                <div className="container navbarContent">
+                    <div className="logoContent">
+                        <img src="bannerlogo.jpg" alt="help-me-heal-logo" style={{ borderRadius: "0.2rem", height: "2rem" }} />
+                        <span style={{
+                            whiteSpace: "nowrap",
+                            marginLeft: "1rem",
+                            marginTop: "0.3rem",
+                            paddingRight: "30%",
+                            color: "rgb(142,149,160)"
+                        }}><b>Help Me Heal</b></span>
+                    </div>
                     <SignInOut isMobile={isMobile} />
                 </div>
             </div>
 
-            <Divider style={{width:"60%", margin:"0 auto"}}/>
+            <Divider style={{ width: "80%", margin: "0 auto", paddingLeft: "50px", paddingRight: "50px" }} />
 
-            <Grow in={true} {...({ timeout: 1000 })}>
+            {/* <Grow in={true} {...({ timeout: 1000 })}>
                 <div className="mainTitle">
                     <img src="bannerlogo.jpg" alt="help-me-heal-logo" style={{borderRadius:"0.5rem", height:"50%", marginTop:"2.8%"}}/>
                     <h2>Help Me Heal</h2>
                 </div>
-            </Grow>
+            </Grow> */}
+            <div className="container">
+                <div className="contentPage">
+                    <Grow in={true} {...({ timeout: 1500 })}>
+                        <div className="content1" >
+                            <h1> Help <br />patient heal <br /> keep contact.</h1>
+                            <div className="subtext">
+                                This is an app that help patient keep contact with his or hers doctors.
+                            </div>
+                        </div>
+                    </Grow>
 
-            <Grow in={true} {...({ timeout: 1500 })}>
-                <div className="content1" style={{marginTop:"2rem", marginBottom:"10rem"}}>
-                    <h1> Help <br/> patient heal <br/> keep contact.</h1>
+                    <Grow in={true} {...({ timeout: 2000 })}>
+                        <div className="demo-img">
+                            <img src="Demo.png" alt="app-demo" />
+                        </div>
+                    </Grow>
                 </div>
-            </Grow>
-
-            <Grow in={true} {...({ timeout: 2000 })}>
-                <div style={{paddingBottom:"10rem"}}>
-                    <img src="Demo.png" alt="app-demo" style={{borderRadius:"0.5rem", width:"80%", height:"100%"}}/>
-                </div>
-            </Grow>
-
+            </div>
         </div>
     )
 }
