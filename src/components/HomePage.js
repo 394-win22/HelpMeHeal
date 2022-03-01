d2dffe2400ced156390eb61b881c8dff2f92f9a4
zacdeng <zacdeng0720@gmail.com>
2022-02-28 22:38:59 -0600

    set icon on to do list clickable

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,7 +4,7 @@ import { SwitchDay } from './SwitchDay'
 import PatientGraphs from "./PatientGraphs";
 import ToDoList from "./ToDoList";
 
-const HomePage = ({activeIndex, setActiveIndex, data, currentDay, surgeryType, name, isMobile, setCurrentDay, setZoom, zoom, user}) => {
+const HomePage = ({activeIndex, setActiveIndex, data, currentDay, surgeryType, name, isMobile, setCurrentDay, setZoom, zoom, user, setPage}) => {
 
     return (
         <div>
@@ -13,7 +13,7 @@ const HomePage = ({activeIndex, setActiveIndex, data, currentDay, surgeryType, n
             <ProgressIndicator activeIndex={activeIndex} setActiveIndex={setActiveIndex} currentDay={currentDay} phaseEndDay={data["surgery"][surgeryType]["phaseEndDay"]} isMobile={isMobile} setZoom={setZoom} zoom={zoom}/>
             <SwitchDay currentDay={currentDay} setCurrentDay={setCurrentDay} isMobile={isMobile} phaseEndDay={data["surgery"][surgeryType]["phaseEndDay"]} />
             <Welcome activeIndex={activeIndex} username={name} surgeryType={surgeryType} firebaseData={data} currentDay={currentDay} daysDict={data["surgery"][surgeryType]["days"]} phaseEndDay={data["surgery"][surgeryType]["phaseEndDay"]} />
-            <ToDoList />
+            <ToDoList setPage={setPage}/>
             <PatientGraphs patientInfo={user}/>
         </div>
     )
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -8,22 +8,22 @@ import Checkbox from '@mui/material/Checkbox';
 import FactCheckIcon from '@mui/icons-material/FactCheck';
 import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
 
-const ToDoList = () => {
+const ToDoList = ({setPage}) => {
     return (
-        <div>
+        <div style={{marginTop:"4%", marginLeft:"40%"}}>
             <List
                 sx={{ width: '100%', maxWidth: 360, bgcolor: "rgb(243, 245, 247)", borderRadius: "2rem"}}
                 subheader={<ListSubheader sx={{bgcolor: "rgb(243, 245, 247)", borderRadius: "2rem"}}> To Do List </ListSubheader>}
             >
                 <ListItem>
-                    <Checkbox />
+                    <Checkbox onChange={setPage("survey")}/>
                     <ListItemText id="switch-list-label-survey" primary="Survey" />
                     <ListItemIcon>
                         <FactCheckIcon />
                     </ListItemIcon>
                 </ListItem>
                 <ListItem>
-                    <Checkbox />
+                    <Checkbox onChange={setPage("playVideo")}/>
                     <ListItemText id="switch-list-label-survey" primary="Video" />
                     <ListItemIcon>
                         <PlayCircleFilledWhiteIcon />
