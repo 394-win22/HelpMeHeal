19abee1a255baa832124d32671dd91ace01e1b98
kaixin1326 <kaixinzheng2022@u.northwestern.edu>
2022-03-01 02:00:52 -0600

    Restyle welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -23,16 +23,13 @@ const Welcome = ({ phase, username, surgeryType, firebaseData, currentDay, daysD
                     })
                 )
             })
-        swal(`Welcome back ${username ? username : "Nobody"}  Today you are on phase ${phase}, day ${progressComplete
-            ? phaseEndDay[Object.entries(phaseEndDay).length]
-            : currentDay} of your ACL recovery.${welcomeMsg}`)
-
+        swal(welcomeMsg)
     }
 
     const buttonStyle = (isMobile) => ({
         mx: 2,
         fontSize: isMobile ? '3vw' : "0.8rem",
-        width: isMobile ? '12vw' : '6rem',
+        width: isMobile ? '12vw' : '10rem',
         margin: '1%',
         bgcolor: "#b43434",
         borderRadius: 2,
@@ -51,38 +48,39 @@ const Welcome = ({ phase, username, surgeryType, firebaseData, currentDay, daysD
         //Better to use a modal instead of using swal?
         popupWelcomeMsg();
         localStorage.setItem("lastLoginDay", currentDay);
-        // return (
-        //     <Grow in={true} {...({ timeout: 1500 })}>
-        //         <div style={{ width: '55%', marginLeft: '23%', marginBottom: "10%" }}>
-        //             <div style={{ color: '#b43434', fontSize: 25, marginBottom: '4rem', marginTop: '4rem' }}>
-        //                 <h2 style={{ textAlign: 'left' }}> Welcome back {username ? username : "Nobody"}, </h2>
-        //             </div>
-
-        //             <div style={{ textAlign: 'left', fontSize: '1.9rem' }}>
-        //                 Today you are on <b>phase {phase}, day {progressComplete
-        //                     ? phaseEndDay[Object.entries(phaseEndDay).length]
-        //                     : currentDay}</b> of your ACL recovery. <br /> <br />
-        //                 {Object.entries(firebaseData.surgery) // First: entry 'ACL'
-        //                     .filter(data => data[0] === surgeryType)
-        //                     .map(data => {
-        //                         return (Object.entries(data[1].days) // Second: entry phase
-        //                             .filter(days => days[0] === daysHasMessage)
-        //                             .map((msg, i) => {
-        //                                 return (
-        //                                     <span key={i}>{msg[1].message}</span>
-        //                                 )
-        //                             })
-        //                         )
-        //                     }
-
-        //                     )
-        //                 } <br /> <br />
-        //                 Remember to take the daily survey to ensure your recovery is on track.
-        //             </div>
-        //         </div>
-        //     </Grow>)
     }
-    return <Button sx={() => buttonStyle(isMobile)} onClick={() => { popupWelcomeMsg() }} >Show Welcome Message</Button>
+    return (
+        <Grow in={true} {...({ timeout: 1500 })}>
+            <div style={{ width: '55%', marginLeft: '23%', marginBottom: "10%" }}>
+                <div style={{ color: '#b43434', fontSize: 25, marginBottom: '4rem', marginTop: '4rem' }}>
+                    <h2 style={{ textAlign: 'left' }}> Welcome back {username ? username : "Nobody"}, </h2>
+                </div>
+
+                <div style={{ textAlign: 'left', fontSize: '1.9rem' }}>
+                    Today you are on <b>phase {phase}, day {progressComplete
+                        ? phaseEndDay[Object.entries(phaseEndDay).length]
+                        : currentDay}</b> of your ACL recovery. <br /> <br />
+                    {/* {Object.entries(firebaseData.surgery) // First: entry 'ACL'
+                        .filter(data => data[0] === surgeryType)
+                        .map(data => {
+                            return (Object.entries(data[1].days) // Second: entry phase
+                                .filter(days => days[0] === daysHasMessage)
+                                .map((msg, i) => {
+                                    return (
+                                        <span key={i}>{msg[1].message}</span>
+                                    )
+                                })
+                            )
+                        }
+
+                        )
+                    } */}
+                    Remember to take the daily survey to ensure your recovery is on track.
+                    <br /><br />
+                </div>
+                <Button sx={() => buttonStyle(isMobile)} onClick={() => { popupWelcomeMsg() }} >Welcome Message</Button>
+            </div>
+        </Grow>)
 }
 
 export default Welcome;
