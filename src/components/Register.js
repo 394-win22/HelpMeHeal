f6b0bae6c30ea12daacd02ec9b700afb19e85fa3
mayalundell <75233949+mayalundell@users.noreply.github.com>
2022-03-01 19:58:33 -0600

    make invalid patient code case apparent

--- src/components/Register.js
+++ src/components/Register.js
@@ -8,6 +8,8 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 
+
+
 const buttonStyle = {
     mx: 2,
     width: 1 / 14,
@@ -23,38 +25,46 @@ const buttonStyle = {
     },
 }
 
-const ValidatePatientCode = (patientCode, userData) => {
-    let patientCodeParse = patientCode.split("+")
-    // console.log(patientCodeParse[0])
-    // console.log(userData[patientCodeParse[0]])
-    return userData[patientCodeParse[0]] && userData[patientCodeParse[0]]['userType'] === 'doctor' && patientCodeParse[1] === 'acl'
-}
 
-const onClickRegister = (googleUser, type, patientCode, userData) => {
-    // TODO remove default values
-    // const patientCode = document.querySelector('#patientCode').value;
-    let patientCodeParse = patientCode.split("+")
-    const uid = googleUser?.uid;
-    if (ValidatePatientCode(patientCode, userData) && type === 'patient') {
-        setData(`/user/${uid}/userType`, type);
-        setData(`/user/${uid}/name`, googleUser?.displayName);
-        setData(`/user/${uid}/startDate`, Date.now());
-        setData(`/user/${uid}/email`, googleUser?.email);
-        setData(`/user/${uid}/doctorId`, patientCodeParse[0]);
-        setData(`/user/${uid}/surgeryType`, patientCodeParse[1]);
-        setData(`/user/${patientCodeParse[0]}/patientId/${uid}`, googleUser?.displayName)
-    }
-    if (type === 'doctor') {
-        setData(`/user/${uid}/userType`, type);
-        setData(`/user/${uid}/name`, googleUser?.displayName);
-        setData(`/user/${uid}/email`, googleUser?.email);
-    }
-}
 
 const RegisterPage = ({ googleUser }) => {
     const [userType, setUserType] = useState("patient");
     const [textValue, setTextValue] = useState("")
     const [userData, loadingData, errorData] = useData("/user");
+    const [validPatientCode, setValidPatientCode] = useState(true);
+
+    const ValidatePatientCode = (patientCode, userData) => {
+        let patientCodeParse = patientCode.split("+")
+        // console.log(patientCodeParse[0])
+        // console.log(userData[patientCodeParse[0]])
+        return userData[patientCodeParse[0]] && userData[patientCodeParse[0]]['userType'] === 'doctor' && patientCodeParse[1] === 'acl'
+    }
+    
+    const onClickRegister = (googleUser, type, patientCode, userData) => {
+        // TODO remove default values
+        // const patientCode = document.querySelector('#patientCode').value;
+        let patientCodeParse = patientCode.split("+")
+        const uid = googleUser?.uid;
+        if (ValidatePatientCode(patientCode, userData) && type === 'patient' && patientCode?.length > 0) {
+            setData(`/user/${uid}/userType`, type);
+            setData(`/user/${uid}/name`, googleUser?.displayName);
+            setData(`/user/${uid}/startDate`, Date.now());
+            setData(`/user/${uid}/email`, googleUser?.email);
+            setData(`/user/${uid}/doctorId`, patientCodeParse[0]);
+            setData(`/user/${uid}/surgeryType`, patientCodeParse[1]);
+            setData(`/user/${patientCodeParse[0]}/patientId/${uid}`, googleUser?.displayName)
+        } 
+        else if (type === 'doctor') {
+            setData(`/user/${uid}/userType`, type);
+            setData(`/user/${uid}/name`, googleUser?.displayName);
+            setData(`/user/${uid}/email`, googleUser?.email);
+        }
+
+        setValidPatientCode(userType === 'patient' && ValidatePatientCode(patientCode, userData) && patientCode?.length > 0);
+    }
+
+    
+
     useEffect(() => {
         if (userData === undefined) return;
 
@@ -80,16 +90,19 @@ const RegisterPage = ({ googleUser }) => {
                 variant="outlined"
                 required
                 disabled={userType === 'doctor' ? true : false}
-                error={userType === 'patient' ? !ValidatePatientCode(textValue, userData) : false}
-                helperText='Please enter code provided by doctor.'
+                error={!validPatientCode}
+                helperText={!validPatientCode ? 'Invalid patient code.' : 'Please enter code provided by doctor.'}
                 onChange={e => {
                     setTextValue(e.target.value)
                 }}
             />
             <br />
-            <Button onClick={() => onClickRegister(googleUser, userType, textValue, userData)} sx={buttonStyle}>
-                register
-            </Button>
+            <div>
+                <Button onClick={() => onClickRegister(googleUser, userType, textValue, userData)} sx={buttonStyle}>
+                    register
+                </Button>
+            </div>
+
         </div>
     )
 }
