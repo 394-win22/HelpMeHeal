fe6cd1887aee5e9611c2068b732720fc48d71eee
kaixin1326 <kaixinzheng2022@u.northwestern.edu>
2022-03-02 18:48:12 -0600

    Add popup alert for videocheck

--- src/components/PlayVideo.js
+++ src/components/PlayVideo.js
@@ -2,7 +2,7 @@ import React from "react";
 import YoutubeEmbed from "./YouTubeEmbed";
 import Button from '@mui/material/Button';
 import Grow from '@mui/material/Grow';
-
+import swal from 'sweetalert';
 const buttonStyle = () => ({
     mx: 2,
     fontSize: '1.1vw',
@@ -19,13 +19,37 @@ const buttonStyle = () => ({
     },
 })
 
+const showPopupAlert = () => {
+    swal({
+        title: "Have you finished today's exercise？",
+        text: "Please make sure you have finished today's exercise!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+        .then((done) => {
+            if (done) {
+                swal("Congradulations for finishing exercise and keep going!", {
+                    icon: "success",
+                });
+                localStorage.setItem("videoCheck", true)
+            } else {
+                swal("Completing the exercises will help you heal, please keep doing it！", {
+                    icon: "warning",
+                });
+                localStorage.setItem("videoCheck", false)
+            }
+        });
+}
+
 const PlayVideo = ({ phase, data }) => {
+
     return (
         <Grow in={true} {...({ timeout: 1500 })}>
             <div>
                 <h1>Please do these exercises to go along with phase {phase}:</h1>
                 <YoutubeEmbed embedId={data["videoUrls"][phase - 1]}></YoutubeEmbed>
-                <Button variant="contained" sx={() => buttonStyle()} onClick={() => localStorage.setItem("videoCheck", true)}>Done</Button>
+                <Button variant="contained" sx={() => buttonStyle()} onClick={() => showPopupAlert()}>Done</Button>
             </div>
         </Grow>
     );
