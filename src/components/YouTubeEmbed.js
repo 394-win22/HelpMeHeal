d54ea2a442937aa877150e8666c4f8a41688afb5
Sathvik <sathu.sathvik@gmail.com>
2022-03-07 19:49:45 -0600

    Added timestamps to videos

--- src/components/YouTubeEmbed.js
+++ src/components/YouTubeEmbed.js
@@ -2,12 +2,22 @@ import React from "react";
 import './YouTubeEmbed.css';
 
 
-const YoutubeEmbed = ({ embedId }) => (
-  <div className="video-responsive">
+const YoutubeEmbed = ({ embedId, start, end }) => {
+  let url ="";
+  if(start === undefined || end === undefined){
+    url = `https://www.youtube.com/embed/${embedId}`;
+  }else{
+    url = `https://www.youtube.com/embed/${embedId}?start=${start}&end=${end}`;
+  }
+
+  console.log(url);
+
+  return (
+    <div className="video-responsive">
     <iframe
       width="853"
       height="480"
-      src={`https://www.youtube.com/embed/${embedId}`}
+      src={url}
       //src={`https://www.youtube.com/embed/ZZ0AENyvytk`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -15,7 +25,8 @@ const YoutubeEmbed = ({ embedId }) => (
       title="Embedded youtube"
     />
   </div>
-);
+  );
+}
 
 // YoutubeEmbed.propTypes = {
 //   embedId: PropTypes.string.isRequired
