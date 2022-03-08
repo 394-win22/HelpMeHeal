50e715bd059eac45f5284ed5b319d92280d5128d
danielpetikian1 <danielpetikian2023@u.northwestern.edu>
2022-03-07 19:07:17 -0600

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,10 +2,31 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
+import {useData, useUserState} from './utilities/firebase'
 import App from './App';
 
-test('renders Help Me Heal link', () => {
+//jest.mock('./utilities/firebase');
+
+it('shows Sign In if not logged in', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Help Me Heal/i);
-  expect(linkElement).toBeInTheDocument();
+  const button = screen.queryByText(/SIGN IN/i);
+  expect(button).toBeInTheDocument();
+});
+
+test('renders Help Me Heal link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Help Me Heal/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('fails if no logged in user', () => {
+    render(<App />);
+    const linkElement = screen.queryByText(/SIGN OUT/i);
+    expect(linkElement).not.toBeInTheDocument();
+});
+
+test('mock user', () => {
+    render(<App />);
+    const linkElement = screen.queryByText(/SIGN OUT/i);
+    expect(linkElement).not.toBeInTheDocument();
 });
