ec8ce71d0cdf6a7904d731a043edcd6a6ad107cc
kaixin1326 <kaixinzheng2022@u.northwestern.edu>
2022-03-08 19:18:20 -0600

    Use fixed

--- src/components/PatientGraphs.js
+++ src/components/PatientGraphs.js
@@ -136,7 +136,7 @@ const PatientGraphs = ({ patientInfo, isMobile, currentDay }) => {
 
     return (
         <Grow in={true} {...({ timeout: 1500 })}>
-            <div className="Graph" style={{ marginBottom: "6rem" }}>
+            <div className="Graph">
                 <div style={isMobile ?
                     { width: "100%", height: "30%", margin: "5% 15% 7% 0%", float: "left" } :
                     { width: "40%", height: "30%", margin: "5% 10% 7% 14%", float: "left" }}>
