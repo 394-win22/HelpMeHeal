ab4c2785df439bab0fe6a4f76f6d43ce2bfa0cc7
kaixin1326 <kaixinzheng2022@u.northwestern.edu>
2022-03-11 00:02:13 -0600

    Update CreateFakePatient.js

--- src/doctor_components/CreateFakePatient.js
+++ src/doctor_components/CreateFakePatient.js
@@ -23,19 +23,17 @@ function getRandomInt(max, offset) {
 
 const FakeFirstName = ["Peter", "Matthew", "Thomas", "Nancy", "Francesca", "Matthew"];
 const FakeLastName = ["Smith", "Johnson", "Quinn", "Adams", "Redding", "Johnson"];
-
+const concerns = ["I can't finish the rehab exercise, because it is so painful.", "I don't know how to do. Help me plz", "I think it is too hard.", "My knee is painful!"]
 const CreateFakeData = ({ googleUser }) => {
+
     const id = "Fake" + getRefByPush('/');
     const fakesurveydata0 = { pain_rating: getRandomInt(3, 0), rehab_successful: 'Yes', concerns: "No" }
-    const fakesurveydata1 = {
-        pain_rating: getRandomInt(4, 7), rehab_successful: 'No', concerns: "Yes", concerns_description: "It finish the rehab exercise, because it is so painful."
-    }
+    const fakesurveydata1 = { pain_rating: getRandomInt(4, 7), rehab_successful: 'No', concerns: "Yes", concerns_description: concerns[getRandomInt(3, 0)] }
     const fakesurveydata2 = { pain_rating: getRandomInt(3, 0), rehab_successful: 'Yes', concerns: "No" }
-    const fakesurveydata3 = { pain_rating: getRandomInt(4, 7), rehab_successful: 'No', concerns: "Yes", concerns_description: "It finish the rehab exercise, because it is so painful." }
-    const fakesurveydata4 = { pain_rating: getRandomInt(4, 7), rehab_successful: 'No', concerns: "Yes", concerns_description: "It finish the rehab exercise, because it is so painful." }
-    const fakesurveydata5 = { pain_rating: getRandomInt(4, 7), rehab_successful: 'No', concerns: "Yes", concerns_description: "It finish the rehab exercise, because it is so painful." }
-    let idx = getRandomInt(5, 0);
-    let name = FakeFirstName[idx] + " " + FakeLastName[idx];
+    const fakesurveydata3 = { pain_rating: getRandomInt(4, 7), rehab_successful: 'No', concerns: "Yes", concerns_description: concerns[getRandomInt(3, 0)] }
+    const fakesurveydata4 = { pain_rating: getRandomInt(4, 7), rehab_successful: 'No', concerns: "Yes", concerns_description: concerns[getRandomInt(3, 0)] }
+    const fakesurveydata5 = { pain_rating: getRandomInt(4, 7), rehab_successful: 'No', concerns: "Yes", concerns_description: concerns[getRandomInt(3, 0)] }
+    let name = FakeFirstName[getRandomInt(5, 0)] + " " + FakeLastName[getRandomInt(5, 0)];
     setData(`/user/${id}/userType`, "patient");
     setData(`/user/${id}/name`, name);
     setData(`/user/${id}/startDate`, Date.now() - 86400000 * 5);
