82f67b6739a2130042b553320fd1ac57cc42f239
kaixin1326 <kaixinzheng2022@u.northwestern.edu>
2022-03-13 09:23:54 -0500

    Change spec test uid to pass test
    Since nobody register I just change uid to existing one.

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -1,38 +1,38 @@
 /* globals cy */
-    
-describe ('Test App', () => {
 
-    it ('launches', () => {
-      cy.visit ('/');
-    });
-    
-    it ('opens login page and checks for help me heal', () => {
-      cy.visit ('/');
-      cy.get('[data-cy=cyLogoContent]').should('contain', 'Help Me Heal');
-    });
+describe('Test App', () => {
 
-    it('logs you in as patient', () => {
-      cy.login("jELJ5w3L4GgroWO22pIawDdHeTi2");
-      cy.visit ('/');
-      cy.get('[data-cy=cySignInButton]').click();
-      cy.get('[data-cy=cyLoggedInPatient]').should('contain','Sathvik')
-    });
-    
-    //logs you out
-    afterEach(() => {
-      cy.logout();
-    });
-
-    it('logs you in as Doctor', () => {
-      cy.login("Yg2OvFEEFJSOKquVD3XKtORiNjB2");
-      cy.visit ('/');
-      cy.get('[data-cy=cySignInButton]').click();
-      cy.get('[data-cy=cyLoggedInDoctor]').should('contain','Kaixin')
-    });
-
-    //logs you out
-    afterEach(() => {
-      cy.logout();
-    });
-
+  it('launches', () => {
+    cy.visit('/');
   });
+
+  it('opens login page and checks for help me heal', () => {
+    cy.visit('/');
+    cy.get('[data-cy=cyLogoContent]').should('contain', 'Help Me Heal');
+  });
+
+  it('logs you in as patient', () => {
+    cy.login("LXVdjGl5pRcYfW3nqUwFR9KtTnx2");
+    cy.visit('/');
+    cy.get('[data-cy=cySignInButton]').click();
+    cy.get('[data-cy=cyLoggedInPatient]').should('contain', 'Sam Zheng')
+  });
+
+  //logs you out
+  afterEach(() => {
+    cy.logout();
+  });
+
+  it('logs you in as Doctor', () => {
+    cy.login("Yg2OvFEEFJSOKquVD3XKtORiNjB2");
+    cy.visit('/');
+    cy.get('[data-cy=cySignInButton]').click();
+    cy.get('[data-cy=cyLoggedInDoctor]').should('contain', 'Kaixin')
+  });
+
+  //logs you out
+  afterEach(() => {
+    cy.logout();
+  });
+
+});
